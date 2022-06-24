8aeff30df2caadc337ef1d2b9defde69ce08887c
TranMinhHieu <95282648+hieu131020@users.noreply.github.com>
2022-06-25 00:19:00 +0700

    update

--- TheGioiDiDong/src/main/java/com/tgdd/service/ProductService.java
+++ TheGioiDiDong/src/main/java/com/tgdd/service/ProductService.java
@@ -1,5 +1,26 @@
 package com.tgdd.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.tgdd.entity.Product;
+import com.tgdd.repository.ProductRepository;
+
+@Service
 public class ProductService {
+
+	@Autowired
+	ProductRepository repo;
+	
+	public Product save(Product product) {
+		return repo.save(product);
+		
+	}
+	public List<Product> getAllProducts() {
+		
+		return repo.findAll();
+	}
+
 
 }
