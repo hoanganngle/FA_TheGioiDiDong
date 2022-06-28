4e7de1a2564c4ab82c176d4314eb8fd4b154a367
TranMinhHieu <95282648+hieu131020@users.noreply.github.com>
2022-06-28 17:00:19 +0700

    update

--- TheGioiDiDong/src/main/java/com/tgdd/entity/Category.java
+++ TheGioiDiDong/src/main/java/com/tgdd/entity/Category.java
@@ -14,10 +14,14 @@ import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Data
+@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "category")
