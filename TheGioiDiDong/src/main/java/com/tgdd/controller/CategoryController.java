f469a8e1bcc6965cafd0737a738278ca3c92e5ff
loctan81 <locvtse141035@fpt.edu.vn>
2022-07-07 00:24:48 +0700

    loc update

--- TheGioiDiDong/src/main/java/com/tgdd/controller/CategoryController.java
+++ TheGioiDiDong/src/main/java/com/tgdd/controller/CategoryController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,45 +18,52 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tgdd.entity.Category;
 import com.tgdd.service.CategoryService;
 
-
 @RestController
 @RequestMapping("categories")
 public class CategoryController {
 	@Autowired
 	private CategoryService categoryService;
 
-	@PostMapping("/")
-	public ResponseEntity<Category> addCategory(@RequestBody Category category) {
-		
-		return new ResponseEntity<>(categoryService.addCategory(category),HttpStatus.OK);
-		
+//add
+	@PostMapping
+	public ResponseEntity<Category> addCategory(@Validated @RequestBody Category category) {
+
+		return new ResponseEntity<>(categoryService.addCategory(category), HttpStatus.OK);
+
 	}
-	
+
+//Update
 	@PutMapping("/{categoryId}")
-    public ResponseEntity<Category> updateCategory(@PathVariable Long categoryId, @RequestBody Category category) {
-        Optional<Category> categoryOptional = categoryService.findCategoriesById(categoryId);
-        return categoryOptional.map(category1 -> {
-            category.setCategoryId(category1.getCategoryId());
-            return new ResponseEntity<>(categoryService.addCategory(category), HttpStatus.OK);
-        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-}
+	public ResponseEntity<Category> updateCategory(@PathVariable Long categoryId, @RequestBody Category category) {
+		Optional<Category> categoryOptional = categoryService.findCategoriesById(categoryId);
+		return categoryOptional.map(category1 -> {
+			category.setCategoryId(category1.getCategoryId());
+			return new ResponseEntity<>(categoryService.addCategory(category), HttpStatus.OK);
+		}).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
+
+//delete
 	@DeleteMapping("/{categoryId}")
-	private ResponseEntity<Category> deleteCategory(@PathVariable Long categoryId){
+	private ResponseEntity<Category> deleteCategory(@PathVariable Long categoryId) {
 		Optional<Category> categoryOptional = categoryService.findCategoriesById(categoryId);
 		return categoryOptional.map(category -> {
 			categoryService.deleteCategory(categoryId);
-			return new ResponseEntity<>(category,HttpStatus.OK);
+			return new ResponseEntity<>(category, HttpStatus.OK);
 		}).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-		
+
 	}
+
+//get all
 	@GetMapping
-    public ResponseEntity<Iterable<Category>> getAllCategory() {
-        return new ResponseEntity<>(categoryService.getAllCategories(), HttpStatus.OK);
-    }
+	public ResponseEntity<Iterable<Category>> getAllCategory() {
+		return new ResponseEntity<>(categoryService.getAllCategories(), HttpStatus.OK);
+	}
+
+//	find by id
 	@GetMapping("/{categoryId}")
-    public ResponseEntity<Category> getCategory(@PathVariable Long categoryId) {
-        Optional<Category> categoryOptional = categoryService.findCategoriesById(categoryId);
-        return categoryOptional.map(category -> new ResponseEntity<>(category, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-}
+	public ResponseEntity<Category> getCategory(@PathVariable Long categoryId) {
+		Optional<Category> categoryOptional = categoryService.findCategoriesById(categoryId);
+		return categoryOptional.map(category -> new ResponseEntity<>(category, HttpStatus.OK))
+				.orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
 }
