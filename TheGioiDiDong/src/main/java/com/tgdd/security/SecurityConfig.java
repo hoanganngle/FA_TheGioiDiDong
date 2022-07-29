1055d12a8cb774a7b71d1b048790a7093ba53854
Hoàng Kim Phú <phuhkse151214@fpt.edu.vn>
2022-07-29 08:05:38 +0700

    Update SecurityConfig.java

--- TheGioiDiDong/src/main/java/com/tgdd/security/SecurityConfig.java
+++ TheGioiDiDong/src/main/java/com/tgdd/security/SecurityConfig.java
@@ -1,30 +1,30 @@
-//package com.tgdd.security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {    
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests().antMatchers("/login").permitAll()
-//                .antMatchers("/add/**", "/edit/**").hasRole("Admin")
-//                .hasAnyRole("Admin", "Collaborator")
-//                .anyRequest().authenticated()
-//                .and().formLogin()
-//                .loginPage("/login")
-//                    .usernameParameter("email")
-//                    .permitAll()
-//                .and()
-//                .rememberMe().key("AbcdEfghIjklmNopQrsTuvXyz_0123456789")
-//                .and()
-//                .logout().permitAll();
-// 
-//        http.headers().frameOptions().sameOrigin();
-//    }
-//}
+package com.tgdd.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/login").permitAll()
+                .antMatchers("/add/**", "/edit/**").hasRole("Admin")
+                .hasAnyRole("Admin", "Collaborator")
+                .anyRequest().authenticated()
+                .and().formLogin()
+                .loginPage("/login")
+                    .usernameParameter("email")
+                    .permitAll()
+                .and()
+                .rememberMe().key("AbcdEfghIjklmNopQrsTuvXyz_0123456789")
+                .and()
+                .logout().permitAll();
+ 
+        http.headers().frameOptions().sameOrigin();
+    }
+}
