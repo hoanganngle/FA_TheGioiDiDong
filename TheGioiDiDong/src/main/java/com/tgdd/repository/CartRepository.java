f46c3215179c071ec9787dde6c99ebcf43447bbc
Hoàng Kim Phú <phuhkse151214@fpt.edu.vn>
2022-07-29 08:05:00 +0700

    fix

--- TheGioiDiDong/src/main/java/com/tgdd/repository/CartRepository.java
+++ TheGioiDiDong/src/main/java/com/tgdd/repository/CartRepository.java
@@ -1,15 +1,15 @@
-//package com.tgdd.repository;
-//import java.util.List;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.stereotype.Repository;
-//
-//import com.tgdd.entity.Cart;
-//
-//@Repository
-//public interface CartRepository extends JpaRepository<Cart, Long> {	
-//	@Query("SELECT c FROM Cart c WHERE c.customer.phone_number = :phone_number")
-//	public List<Cart> findByPhoneNumber(Long phone_number);
-//
-//}
+package com.tgdd.repository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.tgdd.entity.Cart;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {	
+	@Query("SELECT c FROM Cart c WHERE c.customer.phone_number = :phone_number")
+	public List<Cart> findByPhoneNumber(Long phone_number);
+
+}
